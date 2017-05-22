b8dc14eb5176952b7402170ed1327928a083f84e
Emma Stoverink <emmajs16@live.com>
2017-05-22 14:08:01 +0000

    Done.

--- subway_methods.rb
+++ subway_methods.rb
@@ -2,12 +2,12 @@
 
 # You hop on the subway at Union Square. As you are waiting for the train you take a look at the subway map. The map is about 21 inches wide and 35 inches tall. Let's write a method to returns the area of the map:
 def map_size
-  "The map is #{21 * 25} square inches"
+  "The map is #{21*25} square inches"
 end
 
 # Now you give it a shot! It takes about 156 seconds to go between stops and you'll be taking the train for 3 stops. Write a method that returns how long your trip will take.
 def trip_length
-	"It will take #{156*3}"# here
+	"The trip will take #{156*3} seconds."# here
 end
 
 # While you are waiting for the train you decide to play a little dice. Create a method called roll that returns a random number between 1 and 6 (feel free to google "how to generate a random number in ruby").
@@ -20,7 +20,7 @@ end
 
 # The train arrives and you hop on. Guess what time it is? It's showtime! There are 23 people on the train and each person gives the dancers 1.5 dollars. Write a method called pay_dancers that returns how much money they made.
 def pay_dancers
-  "#{1.5*23}"
+  "The dancers were paid #{1.5*23} dollars"
 end
 
 # There is one grumpy lady on the train that doesn't like the dancing though. Write a method called stop_dancing that returns a message to the dancers in all caps.
@@ -30,11 +30,12 @@ end
 
 # There is also a really enthusiastic rider who keeps shouting "Everything is awesome!" Write a method called everything_is_awesome that returns one string with "Everything is awesome!" 5 times. (Ruby has a `.times` method that can help you do this. Think about setting up an empty string and adding to it five times)
 def everything_is_awesome
-  "everything is awesome" 
+"Everything is Awesome! Everything is Awesome! Everything is Awesome! Everything is Awesome! Everything is Awesome!"
+
 end
 # You are almost at your stop and you start thinking about how you are going to get home. You have $18 dollars left on your metro card (For those of you not in New York, the price of a metro card is now $2.75). Write a method called remaining_trips that return how many trips you have left.
 def remaining_trips
-"$ #{18/2.75}"
+"There are #{18/3} trips left on the MetroCard."
 end
 
 # Call your methods below:
